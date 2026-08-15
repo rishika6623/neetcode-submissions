0119c0e6a1f27b7d4e3093a2f476381746793a8d
rishika6623 <rishika6623@gmail.com>
2026-08-15 00:36:42 -0400

    Add: decode-string - submission-2

---
+++ Data Structures & Algorithms/decode-string/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        i = 0
+        total = 0
+        total_char = ""
+        stack = []
+        while i < len(s):
+            if s[i].isdigit():
+                total *= 10
+                total += int(s[i])
+
+            elif s[i].isalpha():
+                total_char += s[i]
+
+            elif s[i] == "[":
+                stack.append(total_char)
+                stack.append(total)
+                total = 0
+                total_char = ""
+
+            elif s[i] == "]":
+                factor = stack.pop()
+                prev_str = stack.pop()
+
+                total_char = prev_str + factor * total_char
+
+            i += 1
+
+        return total_char

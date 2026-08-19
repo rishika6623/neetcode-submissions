4fbf19a1b9ba95eb9c7dd624c9bd8f4b855de6cd
rishika6623 <rishika6623@gmail.com>
2026-08-19 15:45:30 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_dict = Counter(s)
+
+        for letter in t:
+            if letter not in s_dict or s_dict[letter] == 0:
+                return False
+
+            else:
+                s_dict[letter] -= 1
+
+        for key,val in s_dict.items():
+            if val != 0:
+                return False
+
+        return True
+        

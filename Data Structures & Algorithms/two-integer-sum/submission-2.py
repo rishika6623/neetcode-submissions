78ee514374e52efd7ed827f45bef26f1038bc57c
rishika6623 <rishika6623@gmail.com>
2026-08-19 15:50:08 -0400

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #dict = {to find:match index}
+        match = {}
+        for i in range(len(nums)):
+            if nums[i] in match:
+                return [match[nums[i]], i]
+
+            else:
+                match[target - nums[i]] = i
+
